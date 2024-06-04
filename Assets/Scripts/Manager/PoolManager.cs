73ba97bc0771f9787f858aee8792d8f9e9220fcd
재연 <kojy2004@yonsei.ac.kr>
2024-06-04 11:31:42 +0900

    [Update] 병사 오브젝트 풀링

--- Assets/Scripts/Manager/PoolManager.cs
+++ Assets/Scripts/Manager/PoolManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PoolManager : Singleton<PoolManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform m_poolTrans;
+    [SerializeField] GameObject[] m_TroopsPrefabs;
+    Stack<GameObject>[] pool_Troops;
+
+    private void Awake()
     {
-        
+        pool_Troops = new Stack<GameObject>[m_TroopsPrefabs.Length];
+
+        int index = 0;
+        foreach (GameObject troopPrefab in m_TroopsPrefabs)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                GameObject prefab = Instantiate(troopPrefab, m_poolTrans);
+                pool_Troops[index].Push(prefab);
+                prefab.SetActive(false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject Get_Troop(int id)
     {
-        
+        GameObject troop;
+        if(pool_Troops[id].TryPop(out troop)) //²¨³ÂÀ» ¶§
+        {
+            return troop;
+        }
+        else // ²¨³»Áö ¸øÇßÀ» ¶§
+        {
+            GameObject prefab = Instantiate(m_TroopsPrefabs[id], m_poolTrans);
+            return prefab;
+        }
     }
 }
